6a83d77618ff7c6ff5aff539c04fc28d84bcadac
thanh <thanh89508@gmail.com>
2024-06-16 13:06:39 +0700

    modify guest

--- router/roomRouter.js
+++ router/roomRouter.js
@@ -4,28 +4,23 @@ const Room = require('../model/room');
 const User = require('../model/user');
 
 // Create room
-// Create room
 roomRouter.post('/create', async (req, res) => {
     try {
-        const { createdBy } = req.body;
-
-        // Kiểm tra xem người dùng có tồn tại không
+        const { createdBy, playerNumber } = req.body;
         const user = await User.findById(createdBy);
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
 
-        // Tạo phòng mới với chủ phòng là người tạo
         const newRoom = new Room({
             createdBy,
-            players: [createdBy],  // Thêm người tạo vào danh sách người chơi
+            players: [createdBy],
+            playerNumbers: [{ player: createdBy, number: playerNumber }],
             gameStatus: 'waiting',
-            currentTurn: createdBy  // Khởi tạo currentTurn với người chơi đầu tiên
+            currentTurn: createdBy
         });
 
-        // Lưu phòng vào cơ sở dữ liệu
         await newRoom.save();
-
         res.status(201).json(newRoom);
     } catch (error) {
         console.error('Error creating room:', error);
@@ -37,32 +32,31 @@ roomRouter.post('/create', async (req, res) => {
 roomRouter.post('/join', async (req, res) => {
     try {
         const { roomNumber, userId, playerNumber } = req.body;
-        // Kiểm tra xem phòng có tồn tại không bằng số phòng
         const room = await Room.findOne({ roomNumber });
         if (!room) {
             return res.status(404).json({ message: 'Room not found' });
         }
-        // Kiểm tra xem người dùng có tồn tại không
+
         const user = await User.findById(userId);
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-        // Kiểm tra xem phòng đã đủ người chưa
+
         if (room.players.length >= 2) {
             return res.status(400).json({ message: 'Room is full' });
         }
-        // Kiểm tra xem người chơi đã tồn tại trong phòng chưa
+
         if (room.players.includes(userId)) {
             return res.status(400).json({ message: 'User already in room' });
         }
-        // Thêm người chơi vào phòng
+
         room.players.push(userId);
         room.playerNumbers.push({ player: userId, number: playerNumber });
-        // Cập nhật trạng thái game nếu đủ người
+
         if (room.players.length === 2) {
             room.gameStatus = 'ongoing';
         }
-        // Lưu phòng vào cơ sở dữ liệu
+
         await room.save();
         res.status(200).json(room);
     } catch (error) {
@@ -70,117 +64,169 @@ roomRouter.post('/join', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
-// Leave room
 
+// Leave room
 roomRouter.post('/leave', async (req, res) => {
     try {
-        const { roomId, userId } = req.body;
-        // Kiểm tra xem phòng có tồn tại không
-        const room = await Room.findById(roomId);
+        const { roomNumber, userId } = req.body;
+        const room = await Room.findOne(roomNumber);
         if (!room) {
             return res.status(404).json({ message: 'Room not found' });
         }
-        // Kiểm tra xem người chơi có tồn tại trong phòng không
+
         if (!room.players.includes(userId)) {
             return res.status(404).json({ message: 'User not in room' });
         }
-        // Xóa người chơi khỏi phòng
-        room.players = room.players.filter(player => player !== userId);
-        room.playerNumbers = room.playerNumbers.filter(player => player.player !== userId);
-        // Lưu phòng vào cơ sở dữ liệu
+
+        room.players = room.players.filter(player => player.toString() !== userId);
+        room.playerNumbers = room.playerNumbers.filter(player => player.player.toString() !== userId);
+
+        if (room.players.length < 2) {
+            room.gameStatus = 'waiting';
+        }
+        //kiểm tra nếu chủ phòng rời phòng thì xóa phòng
+        if (room.createdBy.toString() === userId) {
+            await Room.findByIdAndDelete(room._id);
+            return res.status(200).json({ message: 'Room deleted' });
+        }
+
         await room.save();
         res.status(200).json(room);
     } catch (error) {
         console.error('Error leaving room:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
-);
-// Start game 
+});
+
+// Start game
 roomRouter.post('/start', async (req, res) => {
     try {
         const { roomId } = req.body;
-        // Kiểm tra xem phòng có tồn tại không
         const room = await Room.findById(roomId);
         if (!room) {
             return res.status(404).json({ message: 'Room not found' });
         }
-        // Kiểm tra xem phòng đã đủ người chưa
+
         if (room.players.length < 2) {
             return res.status(400).json({ message: 'Room is not full' });
         }
-        // Kiểm tra xem game đã bắt đầu chưa
+
         if (room.gameStatus !== 'waiting') {
             return res.status(400).json({ message: 'Game already started' });
         }
-        // Bắt đầu game
+
         room.gameStatus = 'ongoing';
-        // Lưu phòng vào cơ sở dữ liệu
         await room.save();
         res.status(200).json(room);
     } catch (error) {
         console.error('Error starting game:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
-);
+});
 
 // Make guess
+// roomRouter.post('/guess', async (req, res) => {
+//     try {
+//         const { roomNumber, userId, number } = req.body;
+//         const room = await Room.findOne({ roomNumber });
+//         if (!room) {
+//             return res.status(404).json({ message: 'Room not found' });
+//         }
+
+//         if (room.gameStatus !== 'ongoing') {
+//             return res.status(400).json({ message: 'Game not started' });
+//         }
+
+//         if (!room.players.includes(userId)) {
+//             return res.status(404).json({ message: 'User not in room' });
+//         }
+
+//         if (room.currentTurn.toString() !== userId) {
+//             return res.status(400).json({ message: 'Not your turn' });
+//         }
+
+//         room.guesses.push({ user: userId, number });
+//         const opponent = room.players.find(player => player.toString() !== userId);
+//         const opponentNumber = room.playerNumbers.find(pn => pn.player.toString() === opponent.toString()).number;
+
+//         if (parseInt(number) === opponentNumber) {
+//             room.gameStatus = 'finished';
+//             room.winner = userId;
+//             await room.save();
+//             return res.status(200).json({
+//                 message: 'Correct guess! Game over.',
+//                 winner: userId,
+//                 room
+//             });
+//         } else {
+//             room.currentTurn = opponent;
+//             await room.save();
+//             return res.status(200).json({
+//                 message: 'Incorrect guess. Turn switched.',
+//                 room
+//             });
+//         }
+//     } catch (error) {
+//         console.error('Error making guess:', error);
+//         res.status(500).json({ message: 'Internal server error' });
+//     }
+// });
 roomRouter.post('/guess', async (req, res) => {
     try {
-        const { roomNumber, userId, number } = req.body;
-        // Kiểm tra xem phòng có tồn tại không bằng số phòng
-        const room = await Room.findOne({ roomNumber });
-        if (!room) {
-            return res.status(404).json({ message: 'Room not found' });
-        }
-        // Kiểm tra xem game đã bắt đầu chưa
-        if (room.gameStatus !== 'ongoing') {
-            return res.status(400).json({ message: 'Game not started' });
-        }
-        // Kiểm tra xem người chơi có tồn tại trong phòng không
-        if (!room.players.includes(userId)) {
-            return res.status(404).json({ message: 'User not in room' });
-        }
-        // Kiểm tra xem có phải lượt của người chơi không
-        if (room.currentTurn.toString() !== userId) {
-            return res.status(400).json({ message: 'Not your turn' });
-        }
-        // Thêm lượt đoán vào phòng
-        room.guesses.push({ user: userId, number });
-        // Kiểm tra xem người chơi đoán đúng số của đối phương chưa
-        const opponent = room.players.find(player => player.toString() !== userId);
-        const opponentNumber = room.playerNumbers.find(pn => pn.player.toString() === opponent.toString()).number;
-        if (parseInt(number) === opponentNumber) {
-            room.gameStatus = 'finished';
-            room.winner = userId;
-            await room.save();
-            return res.status(200).json({
-                message: 'Correct guess! Game over.',
-                winner: userId,
-                room
-            });
-        } else {
-            // Chuyển lượt cho người chơi tiếp theo
-            room.currentTurn = opponent;
-            await room.save();
-            return res.status(200).json({
-                message: 'Incorrect guess. Turn switched.',
-                room
-            });
-        }
+      const { roomNumber, userId, number } = req.body;
+      const room = await Room.findOne({ roomNumber });
+  
+      if (!room) {
+        return res.status(404).json({ message: 'Room not found' });
+      }
+  
+      if (room.gameStatus !== 'ongoing') {
+        return res.status(400).json({ message: 'Game not started' });
+      }
+  
+      if (!room.players.includes(userId)) {
+        return res.status(404).json({ message: 'User not in room' });
+      }
+  
+      if (room.currentTurn.toString() !== userId) {
+        return res.status(400).json({ message: 'Not your turn' });
+      }
+  
+      room.guesses.push({ user: userId, number });
+  
+      const opponent = room.players.find(player => player.toString() !== userId);
+      const opponentNumber = room.playerNumbers.find(pn => pn.player.toString() === opponent.toString()).number;
+  
+      if (parseInt(number) === opponentNumber) {
+        room.gameStatus = 'finished';
+        room.winner = userId;
+        await room.save();
+        return res.status(200).json({
+          message: 'Correct guess! Game over.',
+          winner: userId,
+          result: { winner: userId, result: room.guesses },
+        });
+      } else {
+        room.currentTurn = opponent;
+        await room.save();
+        return res.status(200).json({
+          message: 'Incorrect guess. Turn switched.',
+          result: { currentTurn: room.currentTurn.toString(), result: room.guesses },
+        });
+      }
     } catch (error) {
-        console.error('Error making guess:', error);
-        res.status(500).json({ message: 'Internal server error' });
+      console.error('Error making guess:', error);
+      return res.status(500).json({ message: 'Internal server error' });
     }
-});
+  });
+  
+  
 
 // Get room
 roomRouter.get('/get/:roomId', async (req, res) => {
     try {
-        const { roomId } = req.params;
-        // Kiểm tra xem phòng có tồn tại không
-        const room = await Room.findById(roomId);
+        const { roomNumber } = req.params;
+        const room = await Room.findOne(roomNumber);
         if (!room) {
             return res.status(404).json({ message: 'Room not found' });
         }
@@ -189,8 +235,7 @@ roomRouter.get('/get/:roomId', async (req, res) => {
         console.error('Error getting room:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
-);
+});
 
 // Get all rooms
 roomRouter.get('/get-all', async (req, res) => {
@@ -201,26 +246,23 @@ roomRouter.get('/get-all', async (req, res) => {
         console.error('Error getting all rooms:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
-);
+});
 
-//delete room
+// Delete room
 roomRouter.delete('/delete/:roomId', async (req, res) => {
     try {
         const { roomId } = req.params;
-        // Kiểm tra xem phòng có tồn tại không
         const room = await Room.findById(roomId);
         if (!room) {
             return res.status(404).json({ message: 'Room not found' });
         }
-        // Xóa phòng
+
         await Room.findByIdAndDelete(roomId);
         res.status(200).json({ message: 'Room deleted' });
     } catch (error) {
         console.error('Error deleting room:', error);
         res.status(500).json({ message: 'Internal server error' });
     }
-}
-);
+});
 
 module.exports = roomRouter;
--- server.js
+++ server.js
@@ -3,46 +3,46 @@ const app = express();
 const userRouter = require('./router/userRouter');
 const roomRouter = require('./router/roomRouter');
 const socket = require('socket.io');
-const http = require('http');
 const cors = require('cors');
 
 require('dotenv').config();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routers
 app.use('/user', userRouter);
 app.use('/room', roomRouter);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+
+// Handle 404 errors
+app.use((req, res, next) => {
+    res.status(404).send("Sorry can't find that!");
 });
 
-const hostName = "192.168.1.189";
+const hostName = "192.168.1.32";
 const port = process.env.PORT || 8000;
-const uri = process.env.ATLAS_URI;
 
 const server = app.listen(port, hostName, () => {
     console.log(`Example app listening on: http://${hostName}:${port}`);
 });
-// CORS configuration for socket.io
+
+// Socket.IO configuration
 const io = socket(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://192.168.1.189:8000'],
+        origin: ['http://localhost:3000', 'http://192.168.1.8:8000'],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     }
 });
 
-// connect to socket to join room, play game, guess number lead to win or lose, leave room
-// listen to port 8000
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('A user connected');
+
     socket.on('joinRoom', ({ roomNumber, secretNumber }, callback) => {
         // Replace with actual room validation logic
         const roomExists = true;
@@ -55,39 +55,61 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('createRoom', ({ secretNumber, room }, callback) => {
-        // Here room is generated client-side, so no need to create a new room
-        socket.join(room);
-        console.log(`Room created: roomNumber=${room}, secretNumber=${secretNumber}`);
-        callback({ room });
+    // socket.on('createRoom', ({ secretNumber, room }, callback) => {
+    //     socket.join(room);
+    //     console.log(`Room created: roomNumber=${room}, secretNumber=${secretNumber}`);
+    //     callback({ room });
+    //   });
+    socket.on("create-room", (data) => {
+        const { roomNumber, secretNumber } = data;
+        socket.join(roomNumber);
+        console.log(`Room created: roomNumber=${roomNumber}, secretNumber=${secretNumber}`);
     });
 
     socket.on('disconnect', () => {
-        console.log('a user disconnected');
+        console.log('A user disconnected');
     });
+
     // Join room
     socket.on('join-room', (data) => {
         console.log('Join room:', data);
-        io.emit('join-room', data);
+        socket.join(data.room); // Ensure socket joins the room
+        socket.to(data.room).emit('join-room', data); // Broadcast to all clients in room except sender
     });
+    // socket.on('join-room', async ({ roomId, userId }) => {
+    //     socket.join(roomId);
+    //     const room = await room.findById(roomId).populate('players playerNumbers.player');
+
+    //     if (room) {
+    //         const opponent = room.players.find(player => player._id.toString() !== userId);
+    //         if (opponent) {
+    //             const opponentNumber = room.playerNumbers.find(pn => pn.player.toString() === opponent._id.toString()).number;
+    //             socket.emit('opponent-info', {
+    //                 opponentId: opponent._id,
+    //                 opponentSecretNumber: opponentNumber
+    //             });
+    //         }
+    //     }
+    // });
+
     // Leave room
     socket.on('leave-room', (data) => {
         console.log('Leave room:', data);
-        io.emit('leave-room', data);
+        socket.leave(data.room); // Ensure socket leaves the room
+        socket.to(data.room).emit('leave-room', data); // Broadcast to all clients in room except sender
     });
+
     // Start game
     socket.on('start-game', (data) => {
         console.log('Start game:', data);
-        io.emit('start-game', data);
+        socket.to(data.room).emit('start-game', data); // Broadcast to all clients in room except sender
     });
+
     // Guess number
     socket.on('guess-number', (data) => {
         console.log('Guess number:', data);
-        io.emit('guess-number', data);
+        socket.to(data.room).emit('guess-number', data); // Broadcast to all clients in room except sender
     });
 });
 
-
 module.exports = app;
-
-
